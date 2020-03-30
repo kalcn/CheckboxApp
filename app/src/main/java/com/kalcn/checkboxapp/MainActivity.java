85c5880e04b4f15a264b3faed07c6b4d33a5bc77
Kalcn <zoon3312@gamil.com>
2020-03-30 11:56:58 +0800

    新增程式

--- app/src/main/java/com/kalcn/checkboxapp/MainActivity.java
+++ app/src/main/java/com/kalcn/checkboxapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.kalcn.checkboxapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void button(View view){
+        CheckBox chk;
+        TextView txv;
+        String msg = "";
+
+        int[] id = {R.id.chk1,R.id.chk2,R.id.chk3,R.id.chk4};
+
+        for (int i:id){
+            chk = (CheckBox)findViewById(i);
+
+            if(chk.isChecked())
+                msg += "\n"+chk.getText();
+        }
+        txv = (TextView)findViewById(R.id.textView2);
+        txv.setText(msg);
     }
 }
